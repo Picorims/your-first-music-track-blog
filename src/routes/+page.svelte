adccd0b986d5570a8f21a46d44c53c34355be0bd
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2024-10-26 21:40:06 +0200

    🐞 fix(project): try to fix blogging kit import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,7 +24,5 @@
     */
    
    import { base } from "$app/paths";
+   import { BlogPost } from "svelte-blogging-kit";
 </script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
