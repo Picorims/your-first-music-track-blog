6d5ffc51fde0807f208bf2a7fba5df54c276d59d
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2024-11-04 23:01:32 +0100

    ✨ feat(post 3): wip

--- src/lib/ExternalLink.svelte
+++ src/lib/ExternalLink.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { Snippet } from "svelte";
+	import type { Snippet } from 'svelte';
 
 	/*
     Copyright (c) 2024 Charly Schmidt aka Picorims<picorims.contact@gmail.com>
@@ -9,14 +9,18 @@
     file, You can obtain one at https://mozilla.org/MPL/2.0/.
     */
 
-    interface Props {
-        href: string;
-        children: Snippet;
-    }
+	interface Props {
+		href: string;
+		children?: Snippet;
+	}
 
-    let { href, children }: Props = $props();
+	let { href, children }: Props = $props();
 </script>
 
 <a {href} target="_blank" rel="noopener noreferrer">
-    {@render children()}
+	{#if children}
+		{@render children()}
+	{:else}
+		{href}
+	{/if}
 </a>
