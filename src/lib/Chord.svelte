a4f22748d47f451839f822238cedfda24d976910
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2024-11-05 15:49:18 +0100

    ✨ feat(components): add chord component and snippet

---
+++ src/lib/Chord.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    /*
+    Copyright (c) 2024 Charly Schmidt aka Picorims<picorims.contact@gmail.com>
+    
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+    */
+
+    export let name: string;
+</script>
+
+<span>{name}</span>
+
+<style>
+    span {
+        font-weight: bold;
+        display: inline-block;
+        background-color: #f0f0f0;
+        border-radius: 4px;
+        padding: 0 0.5em;
+    }
+</style>
