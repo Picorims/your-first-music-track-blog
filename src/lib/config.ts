75436a7e387c14257547bfcf927c4641dd47b5b2
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2024-11-10 18:04:51 +0100

    ✨ feat(project): setup dev env for the blogging system

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,4 @@
+import { dev } from "$app/environment";
 import type { Config } from "svelte-blogging-kit";
 
 export const config: Config = {
@@ -7,4 +8,5 @@ export const config: Config = {
     authorDescription: "I'm Charly - an apprentice IT engineer passionate about music. I started teaching myself music composition out of curiosity many years ago after starting to learn playing the guitar. I created this blog to share the knowledge I accumulated throughout the years and will try to give you the basis for starting this journey yourself!",
     noBodySpacing: true,
     urlAllPosts: "https://your-first-music-track.weebly.com/blog",
+    devEnv: dev,
 }
